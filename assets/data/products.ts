6e2c64123fca1f1e851440822df16429fb6da184
Windancerforest <2374917246@qq.com>
2024-12-15 17:38:58 +0800

    Building UI

---
+++ assets/data/products.ts
@@ -0,0 +1,74 @@
+const products = [
+  {
+    id: 1,
+    name: '至尊香肠披萨',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
+    price: 12.99,
+  },
+  {
+    id: 2,
+    name: '红柿蘑菇披萨',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
+    price: 14.99,
+  },
+  {
+    id: 3,
+    name: '超级至尊披萨',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
+    price: 13.47,
+  },
+  {
+    id: 4,
+    name: '玛格丽特比萨',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png',
+    price: 9.9,
+  },
+  {
+    id: 5,
+    name: '素食披萨',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
+    price: 12.99,
+  },
+  {
+    id: 6,
+    name: '夏威夷风情披萨',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png',
+    price: 10.49,
+  },
+  {
+    id: 7,
+    name: '海鲜披萨',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/deluxe.png',
+    price: 16.99,
+  },
+  {
+    id: 8,
+    name: 'BBQ烧鸡披萨',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
+    price: 12.89,
+  },
+  {
+    id: 9,
+    name: '鸡肉培根比萨',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
+    price: 13.99,
+  },
+  {
+    id: 10,
+    name: '溜溜起司',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png',
+    price: 13.29,
+  },
+];
+
+export default products;
